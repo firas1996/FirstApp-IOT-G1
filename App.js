020ec632ca4fd3d905620d2c743bb013f3f58e91
firas hamila <firas.hm.1996@gmail.com>
2024-02-08 09:03:54 +0100

    ss

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Test from "./Components/Test";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={{
+          uri: "https://wallpapers.com/images/hd/splashing-water-gradient-background-mobile-v5hlk4ta55rn5w3x.jpg",
+        }}
+        resizeMode="cover"
+        style={styles.img}
+      >
+        <Text style={styles.txt}>Email</Text>
+        <TextInput style={styles.imp} />
+        <Text style={styles.txt}>Password</Text>
+        <TextInput style={styles.imp} />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +30,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  img: {
+    flex: 1,
+    justifyContent: "center",
+    opacity: 0.4,
+  },
+  txt: {
+    fontSize: 22,
+  },
+  imp: {
+    backgroundColor: "#888",
+    width: "94%",
+    borderWidth: 2,
+    borderColor: "green",
+    borderRadius: 12,
   },
 });
